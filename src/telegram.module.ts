03b85863293151799d54cf232b21b375e630226b
plombir1917 <dfhhggh@list.ru>
2025-03-11 11:05:08 +0300

    feat: telegram controller

--- src/telegram.module.ts
+++ src/telegram.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { TelegramUpdate } from './telegram.update';
+import { TelegramController } from './telagram.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TelegrafModule.forRoot({ token: process.env.TOKEN }),
   ],
+  controllers: [TelegramController],
   providers: [TelegramUpdate],
 })
 export class TelegramModule {}
